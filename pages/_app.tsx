e425407e6a84d5ebfb6954257ee7821a628f18da
CodeDiablos <codediablos@gmail.com>
2022-12-15 00:51:27 +0800

    updatge chain id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Matic;
+const activeChainId = ChainId.Polygon;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
